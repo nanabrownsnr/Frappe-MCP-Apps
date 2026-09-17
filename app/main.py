6858d539994a415c7b9406ed26898ea406df50b1
nana brown <nanabrownsnr@gmail.com>
2026-09-17 14:18:39 +0100

    Allow all CORS methods and headers

--- app/main.py
+++ app/main.py
@@ -90,8 +90,8 @@ middleware = [
     Middleware(
         CORSMiddleware,
         allow_origins=origins,
-        allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
-        allow_headers=["mcp-protocol-version", "mcp-session-id", "Authorization", "Content-Type"],
+        allow_methods=["*"],
+        allow_headers=["*"],
         expose_headers=["mcp-session-id"],
     )
 ]
